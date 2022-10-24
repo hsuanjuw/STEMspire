26eba631be8a72d0dd598cf46ef08e9e6a1c05b9
hsuanju-wang <77294600+hsuanju-wang@users.noreply.github.com>
2022-10-24 16:25:55 -0400

    Update Analytic.cs

--- Assets/Script/Analysis/Analytic.cs
+++ Assets/Script/Analysis/Analytic.cs
@@ -9,6 +9,8 @@ public class Analytic : MonoBehaviour
     public class Data 
     {
         public string eventName;
+        public string eventType;
+        public string choice;
         public string time;
     }
     [Serializable]
@@ -18,22 +20,43 @@ public class Analytic : MonoBehaviour
     }
 
     DataCollection dataCollection;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
     void Start()
     {
         dataCollection = new DataCollection();
+        SaveData("Game Start", "event");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SaveData("Quit Game", "event");
+            SaveIntoJson();
+        }
     }
 
-    public void SaveData(string name)
+    public void SaveData(string name, string type)
+    {
+        Data newData = new Data();
+        newData.eventName = name;
+        newData.eventName = type;
+        newData.time = FormatTime(Time.time);
+
+        dataCollection.allDatas.Add(newData);
+    }
+    public void SaveData(string name, string type, string choice)
     {
         Data newData = new Data();
         newData.eventName = name;
         newData.time = FormatTime(Time.time);
+        newData.eventType = type;
+        newData.choice = choice;
 
         dataCollection.allDatas.Add(newData);
     }
@@ -50,8 +73,9 @@ public class Analytic : MonoBehaviour
         Debug.Log(time);
         int minutes = (int)time / 60;
         int seconds = (int)time % 60;
-        int milliseconds = (int)time * 1000 % 1000;
-        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+        //int milliseconds = (int)time * 1000 % 1000;
+        //return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
 
