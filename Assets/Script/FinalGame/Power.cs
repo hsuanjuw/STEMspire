0fe08d78e2f340056ff1c7ecde66bb602dfa9020
James Finkel <jfinkel@andrew.cmu.edu>
2022-11-02 13:16:49 -0400

    Power now blinks coil

--- Assets/Script/FinalGame/Power.cs
+++ Assets/Script/FinalGame/Power.cs
@@ -45,9 +45,9 @@ public class Power : MonoBehaviour
         if (currentStatus == MiniGameManager.GameStatus.InProgress)
         {
             if(leftFill.fillStatus == PowerFill.FillStatus.NotFilled)
-                LCountDown();
+                Countdown("Left");
             if(rightFill.fillStatus == PowerFill.FillStatus.NotFilled)
-                RCountDown();
+                Countdown("Right");
             if(rightFill.fillStatus == PowerFill.FillStatus.Filled && leftFill.fillStatus == PowerFill.FillStatus.Filled)
                 Succeed();
         }
@@ -139,59 +139,87 @@ public class Power : MonoBehaviour
                 break;
         }
     }
-
-    private void LCountDown()
+    private IEnumerator LeftFlash(SpriteRenderer flashImg)
     {
-        if (leftSwitchTimeRemaining > 0)
+        if (flashImg != null)
         {
-            leftSwitchTimeRemaining -= Time.deltaTime;
-            if (leftSwitchTimeRemaining < 5)
+            while (leftDangerLight.activeSelf && currentStatus == MiniGameManager.GameStatus.InProgress)
             {
-                if (leftDangerLight.activeSelf == false)
-                {
-                    leftDangerLight.SetActive(true);
-                    leftOnLight.SetActive(false);
-                }
-            }
-            leftFill.FillNextImage(Time.deltaTime * fillSpeed);
-        }
-        else
-        {
-            Fail();
-        }
-
-        if (hintTextActive)
-        {
-            Text text = GameObject.Find("LCountDownTxt").GetComponent<Text>();
-            DisplayTime(leftSwitchTimeRemaining, text);  
+                flashImg.color = Color.red;
+                yield return new WaitForSeconds(1f);
+                flashImg.GetComponent<SpriteRenderer>().color = Color.white;
+                yield return new WaitForSeconds(1f);
+            } 
         }
     }
-    
-    private void RCountDown()
+    private IEnumerator RightFlash(SpriteRenderer flashImg)
     {
-        if (rightSwitchTimeRemaining > 0)
+        if (flashImg != null)
         {
-            rightSwitchTimeRemaining -= Time.deltaTime;
-            if (rightSwitchTimeRemaining < 5)
+            while (rightDangerLight.activeSelf && currentStatus == MiniGameManager.GameStatus.InProgress)
             {
-                if (rightDangerLight.activeSelf == false)
-                {
-                    rightDangerLight.SetActive(true);
-                    rightOnLight.SetActive(false);
-                }
-            }
-            rightFill.FillNextImage(Time.deltaTime * fillSpeed);
+                flashImg.color = Color.red;
+                yield return new WaitForSeconds(1f);
+                flashImg.GetComponent<SpriteRenderer>().color = Color.white;
+                yield return new WaitForSeconds(1f);
+            } 
         }
-        else
+    }
+
+    private void Countdown(string _side)
+    {
+        
+        switch (_side)
         {
-            Fail();
+            case "Left":
+                leftSwitchTimeRemaining -= Time.deltaTime;
+                if (leftSwitchTimeRemaining <= 0)
+                {
+                    leftSwitchTimeRemaining = 0;
+                    Fail();
+                }
+                else
+                {
+                    if(leftSwitchTimeRemaining < 10 && leftDangerLight.activeSelf == false)
+                    {
+                        leftDangerLight.SetActive(true);
+                        leftOnLight.SetActive(false);
+                        StartCoroutine(LeftFlash(leftDangerLight.transform.parent.Find("powerCoilYellow").GetComponent<SpriteRenderer>()));
+                    }
+                    leftFill.FillNextImage(Time.deltaTime * fillSpeed);
+                }
+                if (hintTextActive)
+                {
+                    Text text = GameObject.Find("LCountDownTxt").GetComponent<Text>();
+                    DisplayTime(rightSwitchTimeRemaining, text);
+                }
+                break;
+            case "Right":
+                rightSwitchTimeRemaining -= Time.deltaTime;
+                if (rightSwitchTimeRemaining <= 0)
+                {
+                    rightSwitchTimeRemaining = 0;
+                    Fail();
+                }
+                else
+                {
+                    if(rightSwitchTimeRemaining < 10 && rightDangerLight.activeSelf == false)
+                    {
+                        rightDangerLight.SetActive(true);
+                        rightOnLight.SetActive(false);
+                        StartCoroutine(RightFlash(rightDangerLight.transform.parent.Find("powerCoilBlue").GetComponent<SpriteRenderer>()));
+                    }
+                    rightFill.FillNextImage(Time.deltaTime * fillSpeed);
+                }
+                if (hintTextActive)
+                {
+                    Text text = GameObject.Find("RCountDownTxt").GetComponent<Text>();
+                    DisplayTime(rightSwitchTimeRemaining, text);
+                }
+                break;
         }
 
-        if (hintTextActive)
-        {
-            Text text = GameObject.Find("RCountDownTxt").GetComponent<Text>();
-            DisplayTime(rightSwitchTimeRemaining, text);
-        }
+        
     }
 
     private void DisplayTime(float timeToDisplay, Text timeText)
@@ -203,6 +231,9 @@ public class Power : MonoBehaviour
     public void Fail()
     {
         Debug.Log("Power Failed");
+        DisplayTime(0, GameObject.Find("RCountDownTxt").GetComponent<Text>());
+        DisplayTime(0, GameObject.Find("LCountDownTxt").GetComponent<Text>());
+
         currentStatus = MiniGameManager.GameStatus.Failed;
         FindObjectOfType<MiniGameManager>().CallRestart();
     }
