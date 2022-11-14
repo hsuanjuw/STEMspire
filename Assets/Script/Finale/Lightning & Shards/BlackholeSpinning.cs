560fe871442bb9241ce38acf9bd291f8a0c5a824
James Finkel <jfinkel@andrew.cmu.edu>
2022-11-14 16:07:00 -0500

    Organizing Scripts

--- Assets/Script/Finale/Lightning & Shards/BlackholeSpinning.cs
+++ Assets/Script/Finale/Lightning & Shards/BlackholeSpinning.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class BlackholeSpinning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, 50 * Time.deltaTime);
