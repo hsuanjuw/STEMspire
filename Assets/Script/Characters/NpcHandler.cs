eef5412ec587916e293168e99201ac71b905647f
hsuanju-wang <77294600+hsuanju-wang@users.noreply.github.com>
2022-11-16 12:56:34 -0500

    Add summary to scripts

--- Assets/Script/Characters/NpcHandler.cs
+++ Assets/Script/Characters/NpcHandler.cs
@@ -5,6 +5,10 @@ using RPGM.Gameplay;
 
 public class NpcHandler : MonoBehaviour
 {
+    /// <summary>
+    /// Handle NPC being clicked by the player
+    /// </summary>
+ 
     private Sprite npcSprite;
 
     private DialogueSystem dialogueSystem;
@@ -12,6 +16,14 @@ public class NpcHandler : MonoBehaviour
     private Analytic analytic;
 
     private bool hasTask;
+    public enum CharacterPrefs
+    {
+        Bot_Enthusiast_Chat,
+        Parents_Chat,
+        Service_Guide_Chat
+    }
+
+    public CharacterPrefs characterPrefs;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +32,9 @@ public class NpcHandler : MonoBehaviour
         dialogueSystem = GameObject.FindObjectOfType<DialogueSystem>();
         conversation = this.GetComponentInChildren<ConversationScript>();
         npcSprite = GetComponentInChildren<SpriteRenderer>().sprite;
-        if (this.GetComponentInChildren<Task>())
+
+        // See if there is task object in the children 
+        if (this.GetComponentInChildren<Task>()) 
         {
             hasTask = true;
         }
@@ -34,14 +48,22 @@ public class NpcHandler : MonoBehaviour
 
     public void OnMouseDown()
     {
-        //Debug.Log("clicked");
         if (!dialogueSystem.dialogueOpened)
         {
             dialogueSystem.StartDialogue(conversation, hasTask);
+            // Destroy interaction icon if is triggered
             if (this.GetComponentInChildren<InteractionIcon>())
             {
                 Destroy(this.GetComponentInChildren<InteractionIcon>().gameObject);
             }
+
+            // Set Player Prefs after the interaction icon is triggered
+            if (PlayerPrefs.GetInt(characterPrefs.ToString()) == 0) 
+            {
+                PlayerPrefs.SetInt(characterPrefs.ToString(), 1);
+            }
+
+            // Save data to analytic
             analytic.SaveData(this.name + "Clicked", Time.time);
         }
     }
--- Assets/Script/Environment/InteractionIcon.cs
+++ Assets/Script/Environment/InteractionIcon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class InteractionIcon : MonoBehaviour
 {
+    /// <summary>
+    /// Handle InteractionIcon zoomInOut Effect
+    /// </summary>
+    /// 
     public bool isInteracted = false;
     // Start is called before the first frame update
     void Start()
