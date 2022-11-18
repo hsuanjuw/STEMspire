66d8057c984fa3e7d9a067add48efc60978baf24
James Finkel <jfinkel@andrew.cmu.edu>
2022-11-18 13:11:42 -0500

    Core Color Changer

---
+++ Assets/PowerCoreColorChanger.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerCoreColorChanger : MonoBehaviour
+{
+    public enum CoreColor
+    {
+        Blue,
+        Red
+    };
+
+    public CoreColor currentColor;
+
+    private CoreColor _trueColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trueColor = currentColor;
+        UpdateSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentColor != _trueColor)
+            UpdateSprite();
+    }
+
+    public void SetColor(CoreColor c)
+    {
+        currentColor = c;
+    }
+
+    public void UpdateSprite()
+    {
+        switch (currentColor)
+        {
+            case CoreColor.Blue:
+                GetComponent<SpriteRenderer>().color = Color.white;
+                break;
+            case CoreColor.Red:
+                GetComponent<SpriteRenderer>().color = Color.red;
+                break;
+        }
+
+        _trueColor = currentColor;
+    }
+}
