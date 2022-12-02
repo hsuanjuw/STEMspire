0622e206fc3791d6970c3d400ff9bca6aeeb79f5
James Finkel <jfinkel@andrew.cmu.edu>
2022-12-02 10:38:27 -0500

    Video updates

--- Assets/LoadVideo.cs
+++ Assets/LoadVideo.cs
@@ -5,9 +5,20 @@ public class LoadVideo : MonoBehaviour
 {
     public VideoPlayer myVideoPlayer;
     public string videoName;
+    private bool waiting = true;
     void Start()
     {
         string videoUrl= Application.streamingAssetsPath +"/"+videoName+".mp4";
         myVideoPlayer.url = videoUrl;
+        myVideoPlayer.Prepare();
+    }
+
+    void Update()
+    {
+        if (waiting && myVideoPlayer.isPrepared)
+        {
+            waiting = false;
+            myVideoPlayer.Play();
+        }
     }
 }
